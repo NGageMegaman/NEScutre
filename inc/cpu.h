935d38184c36a99b658a06425409ff90385a8b2d
gerard.candon <gerard.candon@est.fib.upc.edu>
2020-04-08 19:34:59 +0200

    som paletes

--- inc/cpu.h
+++ inc/cpu.h
@@ -4,6 +4,8 @@ using namespace std;
 class cpu {
     public:
 	cpu();
+	void execute(uint8_t opcode);
     private:
-	uint8_t regA, regX, regY, regP, regSP, regCP;
+	uint8_t regA, regX, regY, regP, regCP;
+	uint16_t regSP;
 };
--- src/cpu.cpp
+++ src/cpu.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 cpu::cpu() {
-    regX = regY = regA = regP = regSP = regCP = 0x00;
+    regX = regY = regA = regP = regCP = 0x00;
+    regSP = 0x0000;
+}
+
+void cpu::execute(uint8_t opcode) {
+    switch (opcode) {
+	default : 
+	    regCP += 0x02;
+	    break;
+	
+    }
 }
